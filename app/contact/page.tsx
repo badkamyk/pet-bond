8a2c155539865556383a2ef7082f93569e699ee4
badkamyk <slothmateusz@gmail.com>
2022-12-08 12:01:24 +0100

    refactor: added hero content

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,11 +3,9 @@ export default function Contact() {
         <div className="min-h-screen mt-9">
             <section className="w-full max-w-2xl px-6 py-4 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
                 <h2 className="text-3xl font-semibold text-center text-gray-800 dark:text-white">Get in touch</h2>
-
                 <p className="mt-3 text-center text-gray-600 dark:text-gray-400">
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 </p>
-
                 <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
                     <a href="#"
                        className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
@@ -17,10 +15,8 @@ export default function Contact() {
                                   d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
                                   clipRule="evenodd"/>
                         </svg>
-
                         <span className="mt-2">121 Street, NY</span>
                     </a>
-
                     <a href="#"
                        className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
                         <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -31,7 +27,6 @@ export default function Contact() {
 
                         <span className="mt-2">+2499999666600</span>
                     </a>
-
                     <a href="#"
                        className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md dark:text-gray-200 hover:bg-blue-200 dark:hover:bg-blue-500">
                         <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -39,11 +34,9 @@ export default function Contact() {
                             <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
                             <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
                         </svg>
-
                         <span className="mt-2">example@example.com</span>
                     </a>
                 </div>
-
                 <div className="mt-6 ">
                     <div className="items-center -mx-2 md:flex">
                         <div className="w-full mx-2">
@@ -62,19 +55,15 @@ export default function Contact() {
                                 className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                                 type="text"/>
                         </div>
-
-
                         <div className="w-full mx-2 mt-4 md:mt-0">
                             <label
                                 className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">Phone
                                 number</label>
-
                             <input
                                 className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
                                 type="email"/>
                         </div>
                     </div>
-
                     <div className="w-full mt-4">
                         <label
                             className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200">Message</label>
@@ -82,7 +71,6 @@ export default function Contact() {
                         <textarea
                             className="block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"/>
                     </div>
-
                     <div className="flex justify-center mt-6">
                         <button
                             className="px-4 py-2 text-white transition-colors duration-300 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-gray-600">Send
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,12 +8,19 @@ export default function Hero() {
                     <div className="container mx-auto xl:px-32">
                         <div className="grid lg:grid-cols-2 gap-12 flex items-center">
                             <div className="mt-12 lg:mt-0">
-                                <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">Adopt <br/><span
+                                <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-5">Adopt <br/><span
                                     className="text-blue-600">friend for life</span></h1>
-                                <Link className="inline-block px-7 py-3 mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                                   data-mdb-ripple="true" data-mdb-ripple-color="light" href="/adopt" role="button">List of pupils</Link>
-                                <Link className="inline-block px-7 py-3 bg-transparent text-blue-600 font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
-                                   data-mdb-ripple="true" data-mdb-ripple-color="light" href="/donate" role="button">Support us</Link>
+                                <p className="mb-12 text-lg">You can see here all available pupils and adopt them. It&apos;s
+                                    also possible to support them by donating.</p>
+                                <Link
+                                    className="inline-block px-7 py-3 mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                                    data-mdb-ripple="true" data-mdb-ripple-color="light" href="/adopt" role="button">List
+                                    of pupils</Link>
+                                <Link
+                                    className="inline-block px-7 py-3 bg-transparent text-blue-600 font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+                                    data-mdb-ripple="true" data-mdb-ripple-color="light" href="/donate" role="button">Support
+                                    us</Link>
+
                             </div>
                             <div className="mb-12 lg:mb-0">
                                 <img
